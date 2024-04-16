14e1eb236054717ed881a5a7d9794b1391e623a1
Elsa <zeyugao@outlook.com>
2024-04-16 10:51:14 +0800

    Initial Commit

---
+++ MusicBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicBox
+//
+//  Created by Elsa on 2024/4/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicBox/MusicBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicBoxApp.swift
+//  MusicBox
+//
+//  Created by Elsa on 2024/4/16.
+//
+
+import SwiftUI
+
+@main
+struct MusicBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
