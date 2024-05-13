ba585fc2b186956161f6725d951a1b1e43ab52d2
Elsa <zeyugao@outlook.com>
2024-05-13 10:38:58 +0800

    Hide window instead of close

--- MusicBox/MusicBoxApp.swift
+++ MusicBox/MusicBoxApp.swift
@@ -9,6 +9,28 @@ import SwiftUI
 
 @main
 struct MusicBoxApp: App {
+    class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
+        var mainWindow: NSWindow?
+        func applicationDidFinishLaunching(_ notification: Notification) {
+            mainWindow = NSApp.windows[0]
+            mainWindow?.delegate = self
+        }
+        func windowShouldClose(_ sender: NSWindow) -> Bool {
+            mainWindow?.orderOut(nil)
+
+            return false
+        }
+
+        func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool)
+            -> Bool
+        {
+            if !flag {
+                mainWindow?.makeKeyAndOrderFront(nil)
+            }
+            return true
+        }
+    }
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
