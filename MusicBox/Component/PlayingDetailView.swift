fcdfeb166892f52d53d0a725b3e97ba8bea4f358
Elsa <zeyugao@outlook.com>
2024-05-31 22:23:20 +0800

    Scroll with animation

--- MusicBox/Component/PlayingDetailView.swift
+++ MusicBox/Component/PlayingDetailView.swift
@@ -12,54 +12,68 @@ struct LyricView: View {
     var lyric: [CloudMusicApi.LyricLine]
     @EnvironmentObject var playController: PlayController
     @Binding var hasRoma: Bool
-    
+
     @AppStorage("showRoma") var showRoma: Bool = false
     @AppStorage("showTimestamp") var showTimestamp: Bool = false
 
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            VStack(alignment: .leading) {
-                ForEach(
-                    lyric.indices, id: \.self
-                ) { index in
-                    let line = lyric[index]
-                    let currentPlaying = playController.currentLyricIndex == index
+        ScrollViewReader { proxy in
 
-                    VStack(alignment: .leading) {
-                        if showTimestamp {
-                            Text(String(format: "%.2f", line.time))
-                                .lineLimit(1)
-                                .font(.body)
-                                .foregroundColor(.gray)
-                        }
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .leading) {
+                    ForEach(
+                        lyric.indices, id: \.self
+                    ) { index in
+                        let line = lyric[index]
+                        let currentPlaying = playController.currentLyricIndex == index
 
-                        if showRoma, let romalrc = line.romalrc {
-                            Text(romalrc)
+                        VStack(alignment: .leading) {
+                            if showTimestamp {
+                                Text(String(format: "%.2f", line.time))
+                                    .lineLimit(1)
+                                    .font(.body)
+                                    .foregroundColor(.gray)
+                            }
+
+                            if showRoma, let romalrc = line.romalrc {
+                                Text(romalrc)
+                                    .font(.body)
+                                    .foregroundStyle(
+                                        Color(
+                                            nsColor: currentPlaying
+                                                ? NSColor.textColor : NSColor.placeholderTextColor))
+                            }
+
+                            Text(line.lyric)
                                 .font(.title2)
-                                .foregroundStyle(Color(nsColor: currentPlaying ? NSColor.textColor : NSColor.placeholderTextColor))
-                                // .font(currentPlaying ? .title2 : .body)
-                        }
+                                .foregroundStyle(
+                                    Color(
+                                        nsColor: currentPlaying
+                                            ? NSColor.textColor : NSColor.placeholderTextColor))
 
-                        Text(line.lyric)
-                            .font(.title2)
-                            .foregroundStyle(Color(nsColor: currentPlaying ? NSColor.textColor : NSColor.placeholderTextColor))
-                            // .font(currentPlaying ? .title : .body)
-
-                        if let tlyric = line.tlyric {
-                            Text(tlyric)
-                                .font(.title2)
-                                .foregroundStyle(Color(nsColor: currentPlaying ? NSColor.textColor : NSColor.placeholderTextColor))
-                                // .font(currentPlaying ? .title : .body)
+                            if let tlyric = line.tlyric {
+                                Text(tlyric)
+                                    .font(.title2)
+                                    .foregroundStyle(
+                                        Color(
+                                            nsColor: currentPlaying
+                                                ? NSColor.textColor : NSColor.placeholderTextColor))
+                            }
                         }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+
+                        Spacer()
+                            .id(index)
                     }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-
-                    Spacer()
-                        .id(index)
+                }
+                .padding()
+                .onChange(of: playController.currentLyricIndex) { _, newIndex in
+                    withAnimation(.spring) {
+                        proxy.scrollTo(newIndex ?? 0, anchor: .center)
+                    }
                 }
             }
-            .padding()
         }
         .toolbar {
             ToolbarItemGroup(placement: .primaryAction) {
@@ -73,15 +87,6 @@ struct LyricView: View {
                 }
             }
         }
-        // TODO: Scroll with animation
-        .scrollPosition(
-            id: Binding(
-                get: {
-                    playController.currentLyricIndex ?? 0
-                },
-                set: { value in
-                }),
-            anchor: .center)
     }
 }
 
