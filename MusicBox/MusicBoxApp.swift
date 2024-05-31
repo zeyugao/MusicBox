bf9b59fdb7b98ef1323c9507c6721cdc434f9f6f
Elsa <zeyugao@outlook.com>
2024-05-31 22:54:02 +0800

    Add sparkle

--- MusicBox/MusicBoxApp.swift
+++ MusicBox/MusicBoxApp.swift
@@ -6,9 +6,48 @@
 //
 
 import SwiftUI
+import Sparkle
+
+// This view model class publishes when new updates can be checked by the user
+final class CheckForUpdatesViewModel: ObservableObject {
+    @Published var canCheckForUpdates = false
+
+    init(updater: SPUUpdater) {
+        updater.publisher(for: \.canCheckForUpdates)
+            .assign(to: &$canCheckForUpdates)
+    }
+}
+
+// This is the view for the Check for Updates menu item
+// Note this intermediate view is necessary for the disabled state on the menu item to work properly before Monterey.
+// See https://stackoverflow.com/questions/68553092/menu-not-updating-swiftui-bug for more info
+struct CheckForUpdatesView: View {
+    @ObservedObject private var checkForUpdatesViewModel: CheckForUpdatesViewModel
+    private let updater: SPUUpdater
+    
+    init(updater: SPUUpdater) {
+        self.updater = updater
+        
+        // Create our view model for our CheckForUpdatesView
+        self.checkForUpdatesViewModel = CheckForUpdatesViewModel(updater: updater)
+    }
+    
+    var body: some View {
+        Button("Check for Updates…", action: updater.checkForUpdates)
+            .disabled(!checkForUpdatesViewModel.canCheckForUpdates)
+    }
+}
 
 @main
 struct MusicBoxApp: App {
+    private let updaterController: SPUStandardUpdaterController
+    
+    init() {
+        // If you want to start the updater manually, pass false to startingUpdater and call .startUpdater() later
+        // This is where you can also pass an updater delegate if you need one
+        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+    }
+
     class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
         var mainWindow: NSWindow?
         func applicationDidFinishLaunching(_ notification: Notification) {
@@ -37,6 +76,9 @@ struct MusicBoxApp: App {
         }
         .commands {
             SidebarCommands()
+            CommandGroup(after: .appInfo) {
+                CheckForUpdatesView(updater: updaterController.updater)
+            }
         }
         .defaultSize(width: 1000, height: 700)
     }
