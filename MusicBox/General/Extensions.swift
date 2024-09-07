a753af8c1033bd37ec53e0f237f39834d62b309a
Elsa <zeyugao@outlook.com>
2024-09-07 13:59:29 +0800

    Format

--- MusicBox/General/Extensions.swift
+++ MusicBox/General/Extensions.swift
@@ -11,49 +11,45 @@ extension String {
     func subString(from startString: String, to endString: String) -> String {
         var str = self
         if let startIndex = self.range(of: startString)?.upperBound {
-            str.removeSubrange(str.startIndex ..< startIndex)
+            str.removeSubrange(str.startIndex..<startIndex)
             if let endIndex = str.range(of: endString)?.lowerBound {
-                str.removeSubrange(endIndex ..< str.endIndex)
+                str.removeSubrange(endIndex..<str.endIndex)
                 return str
             }
         }
         return ""
     }
-    
+
     func subString(from startString: String) -> String {
         var str = self
         if let startIndex = self.range(of: startString)?.upperBound {
-            str.removeSubrange(self.startIndex ..< startIndex)
+            str.removeSubrange(self.startIndex..<startIndex)
             return str
         }
         return ""
     }
-    
+
     func subString(to endString: String) -> String {
         var str = self
         if let endIndex = self.range(of: endString)?.lowerBound {
-            str.removeSubrange(endIndex ..< str.endIndex)
+            str.removeSubrange(endIndex..<str.endIndex)
             return str
         }
         return ""
     }
 
     var https: String {
-        get {
-            if starts(with: "http://") {
-                return replacingOccurrences(of: "http://", with: "https://")
-            } else {
-                return self
-            }
+        if starts(with: "http://") {
+            return replacingOccurrences(of: "http://", with: "https://")
+        } else {
+            return self
         }
     }
 }
 
 extension URL {
     var https: URL? {
-        get {
-            return URL(string: absoluteString.https)
-        }
+        return URL(string: absoluteString.https)
     }
 }
 
