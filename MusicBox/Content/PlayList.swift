d886be504f3089d2c470a1e3bece79e61854294b
Elsa <zeyugao@outlook.com>
2025-07-26 18:21:06 +0800

    show song name when select audio

--- MusicBox/Content/PlayList.swift
+++ MusicBox/Content/PlayList.swift
@@ -300,9 +300,14 @@ func uploadCloudFile(songId: UInt64, url: URL, userInfo: UserInfo) async throws 
 }
 
 @MainActor
-func selectAudioFile() async -> URL? {
+func selectAudioFile(forSong songTitle: String? = nil) async -> URL? {
     let openPanel = NSOpenPanel()
-    openPanel.prompt = "Select Audio File"
+    if let songTitle = songTitle {
+        openPanel.prompt = "Select Audio File for \"\(songTitle)\""
+        openPanel.message = "Choose an audio file to upload for \"\(songTitle)\""
+    } else {
+        openPanel.prompt = "Select Audio File"
+    }
     openPanel.allowsMultipleSelection = false
     openPanel.canChooseDirectories = false
     openPanel.canChooseFiles = true
@@ -1198,7 +1203,7 @@ extension SongTableViewController {
     @objc private func uploadToCloud(_ sender: NSMenuItem) {
         guard let song = sender.representedObject as? CloudMusicApi.Song else { return }
         Task {
-            if let url = await selectAudioFile() {
+            if let url = await selectAudioFile(forSong: song.name) {
                 onUploadToCloud?(song, url)
             }
         }
